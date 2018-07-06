c39d982fd781a4a4e330ddfcde5eb55c4bdfec64
Olga VIRCHENKO <ovirchen@e1r2p18.unit.ua>
2018-07-06 15:25:55 +0300

    a

--- inc/compress.hpp
+++ inc/compress.hpp
@@ -1,7 +1,12 @@
 #ifndef COMPRESS_H
 #define COMPRESS_H
 
+#include <cstdlib>
 #include <iostream>
+#include <boost/bind.hpp>
+#include <boost/smart_ptr.hpp>
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
 
 using namespace std;
 
