f3e3e4017408a47a2b23f4ae0aeeb75caab6b529
olga <virchenkoolg@gmail.com>
2018-07-11 19:17:47 +0300

    parser

--- src/parcer.cpp
+++ src/parcer.cpp
@@ -4,17 +4,20 @@ int parcer(char *buf, size_t len, socket_ptr sock)
 {
     int paylen;
     int reqcode;
+    unsigned char c;
 
-    if (len < 14) {
+
+    if (len < 8) {
         boost::asio::write(*sock, boost::asio::buffer("Incorrect message length\n", 25));
         return 1;
     }
-    if (atoi(buf) != 5354 || atoi(buf + 4) != 5259) {
+    cout << buf[0] << endl;
+    if (buf[0] != 0x53 || buf[1] != 0x54 || buf[2] != 0x52 || buf[3] != 0x59) {
         boost::asio::write(*sock, boost::asio::buffer("Incorrect magic value\n", 22));
         return 2;
     }
-    paylen = atoi(buf + 9);
-    if ((reqcode = atoi(buf + 14)) < 1 || reqcode > 4) {
+//    paylen = buf[4];
+    if (buf[7] < 0x01 || buf[7] > 0x04) {
         boost::asio::write(*sock, boost::asio::buffer("Incorrect request code\n", 23));
         return 3;
     }
